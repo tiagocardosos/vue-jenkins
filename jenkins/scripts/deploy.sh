f56ef40ae68589a4eade0855d7855906a1691619
Tiago Cardoso <tiagocardosos@gmail.com>
2018-07-24 11:26:55 -0300

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -27,3 +27,10 @@ echo 'Now...'
 echo 'Visit http://159.89.228.239:3000 to see your Node.js/Vue.js application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
+
+set -x
+mkdir ../project1
+mkdir ../project2
+cd ../project1
+touch file1.txt
+set +x
